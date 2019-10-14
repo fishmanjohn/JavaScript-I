4a191f01b241c93025c8730923621198572ddbfb
fishmanjohn <john@emmaandjohnbyvan.com>
2019-10-14 15:05:47 -0700

    function conversion done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,35 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+let myFunction = () =>{
+    console.log("Function was invoked!");
+};
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) =>{
+    return param;
+};
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+let add = (num1,num2)=>{
+    return num1+num2;
+};
+console.log(add(2,3));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+let subtract = (n1,n2) => {
+    return n1-n2;
+};
+console.log(subtract(5,3))
 
 // Stretch
 
